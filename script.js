476959f39543d030c8cc0ab4ed741cabe70d1f97
clement <58054176+clm-msch@users.noreply.github.com>
2023-03-29 03:38:03 +0200

    refonte du projet

--- script.js
+++ script.js
@@ -11,27 +11,47 @@ function updateText() {
 
 setInterval(updateText, 2000);
 
+const sr = ScrollReveal({
+  duration: 1000,
+  delay: 300,
+  easing: 'ease',
+  reset: false,
+});
 
+sr.reveal('.btn-scroll', {
+  origin: 'bottom',
+  distance: '20px',
+  scale: 0.9,
+});
 
+// Fonction pour l'animation de scroll en douceur
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
 
-// const elements = document.querySelectorAll(".anime-title");
-// const words = ["mot 1", "mot 2", "mot 3", "mot 4"];
-// let currentWordIndex = 0;
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
 
-// function updateText() {
-//   for (let element of elements) {
-//     const text = element.textContent;
-//     const lastIndex = text.lastIndexOf(" ");
-//     const lastWord = text.substring(lastIndex);
-//     element.textContent = text.substring(0, lastIndex);
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
 
-//     const newSpan = document.createElement("span");
-//     newSpan.textContent = words[currentWordIndex];
-//     newSpan.classList.add("animate__animated", "animate__flipInX");
+  requestAnimationFrame(animation);
+}
 
-//     element.appendChild(newSpan);
-//   }
-//   currentWordIndex = (currentWordIndex + 1) % words.length;
-// }
-
-// setInterval(updateText, 2000);
+// Ajouter un gestionnaire d'événements au bouton pour déclencher l'animation de scroll en douceur
+var scrollBtn = document.querySelector(".scroll-btn");
+scrollBtn.addEventListener("click", function () {
+  smoothScroll(".content-section", 1000);
+}); 
